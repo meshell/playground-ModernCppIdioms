cac23479ca167bbc5aba692c41fdeadb03eb8bdc
raphael meyer <raphael.akira.meyer@gmail.com>
2017-09-03 08:54:17 +0200

    Try to catch compilation error

--- cpp-project/check_structured_bindings.sh
+++ cpp-project/check_structured_bindings.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 if make structured_binding ; then
-  ./structured_binding | sed -e 's/^/TECHIO> message -c "output" /'
+  ./structured_binding | sed -e 's/^/TECHIO> message -c "Example output" /'
   ./check_structured_bindings
 else
+  echo "TECHIO> message -c \"Compilation\" Compilation error"
   echo "TECHIO> success false"
 fi
 
