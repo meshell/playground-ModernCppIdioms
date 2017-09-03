c2ffecac018322ca08346c62203d489117f33c97
raphael meyer <raphael.akira.meyer@gmail.com>
2017-09-03 08:40:44 +0200

    Add output. Fail on compile error

--- cpp-project/check_structured_bindings.sh
+++ cpp-project/check_structured_bindings.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-make structured_binding && ./check_structured_bindings
+if make structured_binding ; then
+  ./check_structured_bindings | sed -e 's/^/TECHIO> message -c "output" /'
+else
+  echo "TECHIO> success false"
+fi
