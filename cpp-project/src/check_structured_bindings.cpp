4fd5afeedd2a9589c0a18a053bfc3d5314d21fb8
Michel Estermann <estermann.michel@gmail.com>
2017-09-10 01:47:05 +0200

    Changed the structured binding check to look for other use.

--- cpp-project/src/check_structured_bindings.cpp
+++ cpp-project/src/check_structured_bindings.cpp
@@ -18,7 +18,7 @@ int main() {
             if (exists_in_file(pattern, file_path) < 3) {
                 show_success(false);
                 show_message_on_techio("...but", "Did you replace all occurrences?");
-                return 0;
+                return 1;
             }
             const auto pattern_2 = R"(\[\w+\s*,\s*\w+\]\s*:\s*address_book)"s;
             if (exists_in_file(pattern_2, file_path) > 0) {
@@ -26,16 +26,24 @@ int main() {
             } else {
                 show_success(false);
                 show_message_on_techio("...but", "how about the range based for loop?");
+                return 1;
             }
-
+            const auto access_name_pattern = R"(\w+->first)"s;
+            const auto access_address_pattern = R"(\w+->second)"s;
+            if ((exists_in_file(access_name_pattern, file_path) > 0) ||
+                (exists_in_file(access_address_pattern, file_path) > 0)){
+                show_message_on_techio("However", "How about using structured binding to access the name and address?");
+            }
         } else {
             show_success(false);
             show_message_on_techio("Oops! 🐞", "You didn't refactor the code");
             show_message_on_techio("Hint", "Use structured bindings where a tuple, pair or struct is returned.");
+            return 1;
         }
     } catch (const exception& e) {
         show_success(false);
         show_message_on_techio("Oops! 🐞", e.what());
+        return 1;
     }
 
     return 0;
